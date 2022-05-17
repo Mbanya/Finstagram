234b89b45135a87a9435248a8c10f5a49baf9483
William Mbanya <mbanyaw@gmail.com>
2022-05-17 07:06:56 +0300

    Setup App, Create Login Page, Define named routes

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:finstagram/pages/login_page.dart';
+import 'package:finstagram/pages/register_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FinstaGram',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      initialRoute: 'login',
+      routes: {
+        'register': (context) => RegisterPage(),
+        'login': (context) => LoginPage()
+      },
+    );
+  }
+}
